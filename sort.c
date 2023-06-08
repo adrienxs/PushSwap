d7e0909f3c38cfd24e53284675de13f61cb8908c
adrienxs <asirvent.db@gmail.com>
2023-06-08 22:24:44 +0200

    ...

--- sort.c
+++ sort.c
@@ -9,7 +9,7 @@ static void	push_all_save_three(t_stack **stack_a, t_stack **stack_b)
 	stack_size = get_stack_size(*stack_a);
 	pushed = 0;
 	i = 0;
-	while (stack_size > 6 && i < stack_size && pushed < stack_size / 2)
+	while (stack_size > 6 && i++ < stack_size && pushed < stack_size / 2)
 	{
 		if ((*stack_a)->index <= stack_size / 2)
 		{
@@ -18,13 +18,10 @@ static void	push_all_save_three(t_stack **stack_a, t_stack **stack_b)
 		}
 		else
 			do_ra(stack_a);
-		i++;
 	}
-	while (stack_size - pushed > 3)
-	{
+	stack_size = stack_size - pushed;
+	while (stack_size-- > 3)
 		do_pb(stack_a, stack_b);
-		pushed++;
-	}
 }
 
 static void	shift_stack(t_stack **stack_a)
@@ -36,19 +33,13 @@ static void	shift_stack(t_stack **stack_a)
 	lowest_pos = get_lowest_index_position(stack_a);
 	if (lowest_pos > stack_size / 2)
 	{
-		while (lowest_pos < stack_size)
-		{
+		while (lowest_pos++ < stack_size)
 			do_rra(stack_a);
-			lowest_pos++;
-		}
 	}
 	else
 	{
-		while (lowest_pos > 0)
-		{
+		while (lowest_pos-- > 0)
 			do_ra(stack_a);
-			lowest_pos--;
-		}
 	}
 }
 
